e9988e13250ae16740cb8c658ba6493004484bdd
MustafaBasar471 <mustafabasar471@gmail.com>
2022-06-13 22:25:53 +0300

    first commit

---
+++ article/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey("auth.User",on_delete= models.CASCADE,verbose_name="Yazar")
+    title = models.CharField(max_length= 50,verbose_name="Başlık")
+    content = RichTextField()
+    created_date = models.DateField(auto_now_add= True,verbose_name="Oluşturulma tarihi")
+    article_image = models.FileField(default="default.png", blank = True )
+    def __str__(self):
+        return self.title
